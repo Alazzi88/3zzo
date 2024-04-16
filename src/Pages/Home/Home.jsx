a93d86c5153bd3f5aeb54c5b895a9d77c8d751c2
Yahya Alazzi <yahyaalazzi@Yahyas-MacBook-Air.local>
2024-04-16 13:13:47 +0300

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import "./Home.css";
+import React from "react";
+import Photoo from "../Home/photoAndVideo/photoAndVideo";
+import Service from "./service/services";
+import FloatingActionButton from "../../comp/FloatingActionButton/FloatingActionButton";
+import Footer from "../../comp/footer/footer";
+import Projects from "../Projects/Projects";
+import MediaControlCard from "comp/MediaControlCard/MediaControlCard";
+
+const Home = () => {
+  return (
+    <div className="main-bg">
+      {/* <ViewCarouselOutlined/> */}
+      <Photoo />
+      <Service />
+      <Projects />
+      <FloatingActionButton />
+      <MediaControlCard/>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
